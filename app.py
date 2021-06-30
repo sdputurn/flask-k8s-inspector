687459772985bd9542bcd87c5d9956e70df3960f
Sandeep Singh <sandeep.singh03@sap.com>
2021-06-30 19:59:12 +0530

    added sync_check

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import time
 
-import redis
+# import redis
 from flask import Flask
+from flask import request
 from flask import render_template
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+# cache = redis.Redis(host='redis', port=6379)
 
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+# def get_hit_count():
+#     retries = 5
+#     while True:
+#         try:
+#             return cache.incr('hits')
+#         except redis.exceptions.ConnectionError as exc:
+#             if retries == 0:
+#                 raise exc
+#             retries -= 1
+#             time.sleep(0.5)
 
 
 @app.route('/html')
@@ -37,3 +38,16 @@ def base():
     '''
     # count = get_hit_count()
     return 'Hello World! new features will be added soon. there is one more route /html v20'
+@app.route('/sync_check')
+def sync_check():
+    username = request.args.get('username')
+    print("call by -------------------------- ", username)
+    # time.sleep(50)
+    # why localhost:5000/sync_check?username=singh is completed before localhost:5000/sync_check?username=singh1 ? 
+    # why id did not wait for singh1 to complete
+    if username != "singh":
+        time.sleep(50)
+        return username
+    if username == "singh":
+        return username
+    
